9627e6338adbe6f8100ed9c034d6a47c8b7fc8b9
Kai <73109076+stackedflows@users.noreply.github.com>
2021-01-29 16:22:40 +0000

    Update m_perceptron.py

--- m_perceptron.py
+++ m_perceptron.py
@@ -10,40 +10,62 @@ class mlp:
         self.inputs = ins
         self.hidden_layers = h_layers
         self.outputs = outs
-
+        
+        #to be constructed during the forward pass wil be structed as [[,], [,,], [,], [,,], [,]]
+        self.activations_matrix = []
+        
+        #to be constructed during the backpropogation
+        self.weight_derivatives = []
+        
+        #to be initialised
         self.weight_matrix = []
         
         #initialising inputs weight layers
         weight_layer_0 = []
+        derivatives_0 = []
         for i in range(self.inputs):
             partition_0 = []
+            d_partitions_0 = []
             for ii in range(self.hidden_layers[0]):
-                random_weight = 2 * random.random() - 1
+                #to be used after testing: random_weight = 2 * random.random() - 1
                 partition_0.append(1)
+                d_partitions_0.append(0)
             weight_layer_0.append(partition_0)
+            derivatives_0.append(d_partitions_0)
         self.weight_matrix.append(weight_layer_0)
+        self.weight_derivatives.append(derivatives_0)
         
         #initialising hidden layers
         if len(self.hidden_layers) > 1:
             for i in range(1, len(self.hidden_layers)):
                 weight_layer_i_hidden = []
+                derivatives_layer_i_hidden = []
                 for ii in range(self.hidden_layers[i - 1]):
                     partition_i = []
+                    d_partitions_i = []
                     for iii in range(self.hidden_layers[i]):
-                        random_weight = 2 * random.random() - 1
+                        #to be used after testing: random_weight = 2 * random.random() - 1
                         partition_i.append(1)
+                        d_partitions_i.append(0)
                     weight_layer_i_hidden.append(partition_i)
+                    derivatives_layer_i_hidden.append(d_partitions_i)
                 self.weight_matrix.append(weight_layer_i_hidden)
+                self.weight_derivatives.append(derivatives_layer_i_hidden)
                 
         #initialising output weights layer
         weight_layer_last = []
+        derivatives_layer_last = []
         for i in range(self.hidden_layers[len(self.hidden_layers) - 1]):
             partition_last = []
+            d_partitions_last = []
             for ii in range(self.outputs):
-                random_weight = 2 * random.random() - 1
+                #to be used after testing: random_weight = 2 * random.random() - 1
                 partition_last.append(1)
+                d_partitions_last.append(0)
             weight_layer_last.append(partition_last)
+            derivatives_layer_last.append(d_partitions_last)
         self.weight_matrix.append(weight_layer_last)
+        self.weight_derivatives.append(derivatives_layer_last)
         
         return
     
@@ -68,13 +90,15 @@ class mlp:
                 for iii in range(len(self.weight_matrix[i][ii])):
                     partitions_new_activations.append(activations[ii] * self.weight_matrix[i][ii][iii])
                 new_activations.append(partitions_new_activations)
+            self.activations_matrix.append(new_activations)
             dot_activations = []
             for ii in range(len(new_activations) - 1):
                 dot_activations = self.dot(new_activations[ii], new_activations[ii + 1])
             activations = dot_activations
             for ii in range(len(activations)):
-                activations[ii] = self.sigmoid(activations[ii])
+                #activations[ii] = self.sigmoid(activations[ii])
                 pass
+        print(self.activations_matrix)
         return activations
     
     #propogates all input data
@@ -102,9 +126,10 @@ class mlp:
                 errors_i.append(forward_outputs_i[ii] - correct_outputs_i[ii])
             errors.append(errors_i)
         print(errors)
-            
-
+        
+        
+        return
     
-#testing
-MLP = mlp() 
-test = MLP.back_propogate([[.5, 1], [1, .5]], [[1, 1], [1, 1]])
+    def gradient_descent(self, weight_matrix, learning_rate):
+        
+        return
