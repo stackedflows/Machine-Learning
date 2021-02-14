917b073e6586bebb33d0cbdef10cbc9fe6f8bf89
Kai <73109076+stackedflows@users.noreply.github.com>
2021-02-14 02:15:36 +0000

    Update classify_web_cam.cs

--- TFC/classify_web_cam.cs
+++ TFC/classify_web_cam.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Barracuda;
+using System.IO;
+using System.Linq;
 
 public class Webcam : MonoBehaviour
 {
-
     public NNModel mob_net;
     public Model model;
     private IWorker worker;
@@ -15,8 +16,17 @@ public class Webcam : MonoBehaviour
 
     public RawImage display;
 
+    public RawImage output;
+
+    private bool brain_on = false;
+
+    private const int SIZE = 224;
+    private const int MEAN = 127;
+    private const float STD_DEV = 127.5f;
+
     public void start_cam()
     {
+
         model = ModelLoader.Load(mob_net);
         worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
 
@@ -29,38 +39,8 @@ public class Webcam : MonoBehaviour
             WebCamDevice device = WebCamTexture.devices[current_cam_index];
             tex = new WebCamTexture(device.name);
             display.texture = tex;
-
             tex.Play();
         }
-    }
-
-    private void classify()
-    {
-        var channels = 3;
-
-        var cam_pix = tex.GetPixels(
-            (int)display.transform.position.x,
-            (int)display.transform.position.y,
-            (int)tex.width,
-            (int)tex.height
-            );
-
-        Texture2D input = new Texture2D(224, 224);
-        input.SetPixels(cam_pix);
-        input.Apply();
-
-        var in_tensor = new Tensor(input, channels);
-
-        worker.Execute(in_tensor);
-
-        var out_tensor = worker.PeekOutput("output"); 
-        var max_val = Mathf.Max(out_tensor.ToReadOnlyArray());
-        var arr = out_tensor.ToReadOnlyArray();
-        var index = System.Array.IndexOf(arr, max_val);
-
-        UnityEngine.Debug.Log("Output = " + out_tensor[0]);     
-        UnityEngine.Debug.Log("Max prob = " + max_val);
-        UnityEngine.Debug.Log("Index of max = " + index);
     }
 
     public void stop_cam()
@@ -68,5 +48,59 @@ public class Webcam : MonoBehaviour
         display.texture = null;
         tex.Stop();
         tex = null;
+    }
+
+ 
+    void crop_and_normalize(WebCamTexture src)
+    {
+        int x = Mathf.FloorToInt(display.transform.position.x);
+        int y = Mathf.FloorToInt(display.transform.position.y);
+
+        Color[] pix = src.GetPixels(x, y, SIZE, SIZE);
+
+        Texture2D dest = new Texture2D(SIZE, SIZE);
+
+        dest.SetPixels(pix);
+        dest.Apply();
+
+        float[] floats = new float[SIZE * SIZE * 3];
+
+        for (int i = 0; i < pix.Length; ++i)
+        {
+            var color = pix[i];
+
+            floats[i * 3 + 0] = (color.r - MEAN) / STD_DEV;
+            floats[i * 3 + 1] = (color.g - MEAN) / STD_DEV;
+            floats[i * 3 + 2] = (color.b - MEAN) / STD_DEV;
+        }
+
+        Tensor in_tensor = new Tensor(0, SIZE, SIZE, 3, floats);
+
+        worker.Execute(in_tensor);
+
+        Tensor out_tensor = worker.PeekOutput();
+
+        var max = Mathf.Max(out_tensor.ToReadOnlyArray());
+        var arr = out_tensor.ToReadOnlyArray();
+        var index = System.Array.IndexOf(arr, max);
+
+        string line = File.ReadLines(@"D:\Unity\WebCam\Cam\Assets\Scenes\mobile_net.txt").Skip(index).Take(1).First();
+
+        Debug.Log(line);
+    }
+
+    public void brain()
+    {
+        brain_on = !brain_on;
+    }
+
+    private void Update()
+    {
+        if (brain_on)
+        {
+            crop_and_normalize(tex);
+
+            brain_on = false;
+        }
     }
 }
