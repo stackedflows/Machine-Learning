11a7bfcae04b0781c252c50bc3445f6f74f5b91e
Kai <73109076+stackedflows@users.noreply.github.com>
2021-02-18 02:32:27 +0000

    Update LSTM.py

--- LSTM/LSTM.py
+++ LSTM/LSTM.py
@@ -1,1 +1,20 @@
+#python LSTM.py
 
+import torch.nn
+
+#dropout: randomly zeroes out elements of tensor input
+#LSTM: layer of LSTM cells
+#softmax: scales vecotr to [0,1]
+#ReLU: max(0, x)
+#CEL: log average difference
+from torch.nn import Dropout, LSTM, Softmax, ReLU
+from torch.nn import CrossEntropyLoss as CEL
+
+import torchvision.datasets as datasets
+
+mnist = datasets.MNIST(root = './data', train = True, download = True, transform = None)
+
+model = torch.nn.Sequential(
+)
+
+model.train()
