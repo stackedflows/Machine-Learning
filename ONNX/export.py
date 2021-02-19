9a2c5120269231b9ce8bd4fc2e356d0e1c19efbb
Kai <73109076+stackedflows@users.noreply.github.com>
2021-02-19 01:41:03 +0000

    Create export.py

---
+++ ONNX/export.py
@@ -0,0 +1,27 @@
+  
+#python export.py
+import torch
+
+from MNIST import Net
+
+def main():
+    #train and load model
+    pytorch_model = Net()
+    pytorch_model.load_state_dict(torch.load('pytorch_model.pt'))
+    pytorch_model.eval()
+    dummy_input = torch.randn(1, 1, 28, 28)
+    
+    #recommended barracuda export settings
+    torch.onnx.export(pytorch_model,
+                        dummy_input,
+                        'MNIST.onnx',
+                        export_params = True,
+                        opset_version = 9,
+                        do_constant_folding = True,
+                        input_names = ['X'],
+                        output_names = ['Y'],
+                        verbose = True
+                     )
+    
+if __name__ == '__main__':
+    main()
